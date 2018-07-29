07aa1cc4da73559f028f6254263fce26833fe8f4
cain19811028 <cain19811028@gmail.com>
2018-07-29 23:39:36 +0800

    test build test.md

--- build.py
+++ build.py
@@ -2,15 +2,56 @@ from datetime import datetime
 import json
 import requests
 
+head = '''# Framework
+'''
+table = '''
+## {} Framework
+
+| Project Name | Stars | Forks | Description | Last Commit |
+| ------------ | ----- | ----- | ----------- | ----------- |
+'''
+tail = '\n*Update Date: {}*'
+
 repos = list()
+api = 'https://api.github.com/repos/'
 
 def main():
     token = get_token()
-    print(token)
+    with open('web_list.txt', 'r') as f:
+        for url in f.readlines():
+            url = url.strip()
+            if url.startswith('https://github.com/'):
+                print(url)
+                path = '{}{}?access_token={}'.format(api, url[19:], token)
+                r = requests.get(path)
+                repo = json.loads(r.content)
+
+                path = '{}{}/commits/{}?access_token={}'.format(api, url[19:], repo['default_branch'], token)
+                r = requests.get(path)
+                commit = json.loads(r.content)
+
+                repo['last_commit_date'] = commit['commit']['committer']['date']
+                repos.append(repo)
+
+        repos.sort(key=lambda r: r['stargazers_count'], reverse=True)
+        build(repos)
 
 def get_token():
     with open('github_token.txt', 'r') as f:
         return f.read().strip()
+
+def build(repos):
+    with open('test.md', 'w') as f:
+        f.write(head)
+        f.write(table.format('Web'))
+        for repo in repos:
+            f.write('| [{}]({}) | {} | {} | {} | {} |\n'.format(repo['name'],
+                                                                repo['html_url'],
+                                                                repo['stargazers_count'],
+                                                                repo['forks_count'],
+                                                                repo['description'],
+                                                                datetime.strptime(repo['last_commit_date'], '%Y-%m-%dT%H:%M:%SZ').strftime('%Y-%m-%d %H:%M:%S')))
+        f.write(tail.format(datetime.now().strftime('%Y-%m-%dT%H:%M:%S%Z')))
 
 if __name__ == '__main__':
     main()
---
+++ test.md
@@ -0,0 +1,12 @@
+# Framework
+
+## Web Framework
+
+| Project Name | Stars | Forks | Description | Last Commit |
+| ------------ | ----- | ----- | ----------- | ----------- |
+| [flask](https://github.com/pallets/flask) | 37653 | 11073 | The Python micro framework for building web applications. | 2018-07-24 18:04:58 |
+| [django](https://github.com/django/django) | 35357 | 15086 | The Web framework for perfectionists with deadlines. | 2018-07-27 21:15:00 |
+| [tornado](https://github.com/tornadoweb/tornado) | 16243 | 4647 | Tornado is a Python web framework and asynchronous networking library, originally developed at FriendFeed. | 2018-07-29 15:25:32 |
+| [sanic](https://github.com/channelcat/sanic) | 9833 | 913 | Async Python 3.5+ web server that's written to go fast | 2018-07-21 05:31:15 |
+
+*Update Date: 2018-07-29T23:37:54*
